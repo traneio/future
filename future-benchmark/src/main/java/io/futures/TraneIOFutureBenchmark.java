aa2756ff3536341747a2c1c0247d37085a0ee94f
fwbrasil <fwbrasil@gmail.com>
2017-02-07 23:32:55 -0800

    fix warning

--- future-benchmark/src/main/java/io/futures/TraneIOFutureBenchmark.java
+++ future-benchmark/src/main/java/io/futures/TraneIOFutureBenchmark.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-import java.util.stream.Stream;
 
 import org.openjdk.jmh.annotations.Benchmark;
 
