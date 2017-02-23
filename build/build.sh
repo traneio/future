266f7c0a433eea54b72a52f18bf78dd070286be7
fwbrasil <fwbrasil@gmail.com>
2017-02-23 00:57:27 -0800

    build

--- build/build.sh
+++ build/build.sh
@@ -3,15 +3,15 @@ set -e # Any subsequent(*) commands which fail will cause the shell script to ex
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]
 then
-	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/pubring.gpg.enc -out $BUILD_DIR/local.pubring.gpg -d
-	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/secring.gpg.enc -out $BUILD_DIR/local.secring.gpg -d
-	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/deploy_key.pem.enc -out $BUILD_DIR/local.deploy_key.pem -d
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/pubring.gpg.enc -out $BUILD_DIR/pubring.gpg -d
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/secring.gpg.enc -out $BUILD_DIR/secring.gpg -d
+	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/deploy_key.pem.enc -out $BUILD_DIR/deploy_key.pem -d
 	if [ -e "release.properties" ] && [ $TRAVIS_BRANCH == "master" ]
 	then
 		echo "Performing a release..."
 		eval "$(ssh-agent -s)"
-		chmod 600 $BUILD_DIR/local.deploy_key.pem
-		ssh-add $BUILD_DIR/local.deploy_key.pem
+		chmod 600 $BUILD_DIR/deploy_key.pem
+		ssh-add $BUILD_DIR/deploy_key.pem
 		git config --global user.name "TraneIO CI"
 		git config --global user.email "ci@trane.io"
 		git remote set-url origin git@github.com:traneio/future.git
