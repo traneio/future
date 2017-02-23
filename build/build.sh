c3d0b4f279abe826454f19b1a024bb01b2038232
fwbrasil <fwbrasil@gmail.com>
2017-02-22 23:48:39 -0800

    fiix build script

--- build/build.sh
+++ build/build.sh
@@ -6,7 +6,7 @@ then
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/pubring.gpg.enc -out $BUILD_DIR/local.pubring.gpg -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/secring.gpg.enc -out $BUILD_DIR/local.secring.gpg -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/deploy_key.pem.enc -out $BUILD_DIR/local.deploy_key.pem -d
-	if [[ $TRAVIS_BRANCH == "master" && [ -f release.properties ] ]]
+	if [[ $TRAVIS_BRANCH == "master" && -f release.properties ]]
 	then
 		eval "$(ssh-agent -s)"
 		chmod 600 $BUILD_DIR/local.deploy_key.pem
