8c621f7e81f5bd125aa0fdf8cfb0d70f051d7e8f
fwbrasil <fwbrasil@gmail.com>
2017-02-25 00:22:57 -0800

    0.0.14

--- build/build.sh
+++ build/build.sh
@@ -19,13 +19,10 @@ then
 		git fetch --unshallow
 		git checkout master || git checkout -b master
 		git reset --hard origin/master
+		git rm release.version
+		git commit -m "remove release.version"
 		mvn -B clean release:prepare --settings build/settings.xml -DreleaseVersion=$(cat release.version)
 		mvn release:perform --settings build/settings.xml
-		git pull
-		git rm release.properties
-		git rm release.version
-		git commit -m "remove release files"
-		git push
 	elif [[ $TRAVIS_BRANCH == "master" ]]
 	then
 		echo "Publishing a snapshot..."
