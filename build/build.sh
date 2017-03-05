089f08883e5d4176e5102f66e631dbda9d1cd24a
fwbrasil <fwbrasil@gmail.com>
2017-03-05 00:11:56 -0800

    fix branch build

--- build/build.sh
+++ build/build.sh
@@ -7,21 +7,21 @@ then
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/secring.gpg.enc -out $BUILD_DIR/secring.gpg -d
 	openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $BUILD_DIR/deploy_key.pem.enc -out $BUILD_DIR/deploy_key.pem -d
 
-	eval "$(ssh-agent -s)"
-	chmod 600 $BUILD_DIR/deploy_key.pem
-	ssh-add $BUILD_DIR/deploy_key.pem
-	git config --global user.name "TraneIO CI"
-	git config --global user.email "ci@trane.io"
-	git config --global push.default matching
-	git remote set-url origin git@github.com:traneio/future.git
-	git fetch --unshallow
-	git checkout master || git checkout -b master
-	git reset --hard origin/master
-
 	if [ -e "release.version" ] && [ $TRAVIS_BRANCH == "master" ]
 	then
 		echo "Performing a release..."
 		RELEASE_VERSION=$(cat release.version)
+
+		eval "$(ssh-agent -s)"
+		chmod 600 $BUILD_DIR/deploy_key.pem
+		ssh-add $BUILD_DIR/deploy_key.pem
+		git config --global user.name "TraneIO CI"
+		git config --global user.email "ci@trane.io"
+		git config --global push.default matching
+		git remote set-url origin git@github.com:traneio/future.git
+		git fetch --unshallow
+		git checkout master || git checkout -b master
+		git reset --hard origin/master
 
 		git rm release.version
 		git commit -m "[skip ci] [release] remove release.version"
@@ -51,7 +51,8 @@ then
 
 	else
 		echo "Publishing a branch snapshot..."
-		mvn clean versions:set -DnewVersion=$TRAVIS_BRANCH-SNAPSHOT org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar deploy --settings build/settings.xml 
+		mvn clean versions:set -DnewVersion=$TRAVIS_BRANCH-SNAPSHOT
+		mvn org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar deploy --settings build/settings.xml 
 	fi
 else
 	echo "Running build..."
